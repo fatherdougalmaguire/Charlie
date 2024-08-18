de10dade18a4eb84f2c2ea8d7f2ed2e0c858523d
fatherdougalmaguire <144415904+fatherdougalmaguire@users.noreply.github.com>
2024-08-18 21:24:56 +1000

    Initial Commit

---
+++ Charlie/CharlieApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharlieApp.swift
+//  Charlie
+//
+//  Created by Antonio Sanchez-Rivas on 18/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct CharlieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Charlie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Charlie
+//
+//  Created by Antonio Sanchez-Rivas on 18/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
